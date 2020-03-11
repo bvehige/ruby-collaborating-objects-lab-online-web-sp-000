6bb096c0de463bb82de708ba567d728b4cb64cd7
Brian Vehige <brianvehige@gmail.com>
2020-03-11 06:05:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,13 @@
 
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :song
 
   @@all = []
 
   def initialize(name)
     @name = name
     @songs= []
+    @@all << name 
   end
 
   def add_song(song)
