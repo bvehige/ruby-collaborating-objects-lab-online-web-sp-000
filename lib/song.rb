62407954d54d40327078feeea84e5087571702ff
Brian Vehige <brianvehige@gmail.com>
2020-03-11 06:44:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,15 @@ class Song
 
   attr_accessor :name, :artist
 
+ @@all = []
+
   def initialize(name)
     @name = name
+    @@all = self 
   end
 
   def self.all
-    artist.songs
+    @@all 
   end
     
     
