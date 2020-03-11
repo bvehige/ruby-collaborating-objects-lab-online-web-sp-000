dbbad01bf8be2dc61754cd75e46b65ff0357cc52
Brian Vehige <brianvehige@gmail.com>
2020-03-11 05:08:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ def self.new_by_filename(filename)
  song_name = filename.split(" -  ")[1]
  artist = filename.split(" - ")[0]
  song = self.new(song_name)
- artist = 
+ artist_name=(artist)
  song
 end
 
